0581f84b823abdefcb485a447ca9f199e3cea824
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-08-07 09:53:42 +0300

    completed task 5

---
+++ on-boarding/product-5/shrine_app/lib/app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+import 'login.dart';
+
+// TODO: Convert ShrineApp to stateful widget (104)
+class ShrineApp extends StatelessWidget {
+  const ShrineApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shrine',
+      initialRoute: '/login',
+      routes: {
+        '/login': (BuildContext context) => const LoginPage(),
+        // TODO: Change to a Backdrop with a HomePage frontLayer (104)
+        '/': (BuildContext context) => const HomePage(),
+        // TODO: Make currentCategory field take _currentCategory (104)
+        // TODO: Pass _currentCategory for frontLayer (104)
+        // TODO: Change backLayer field value to CategoryMenuPage (104)
+      },
+      // TODO: Customize the theme (103)
+      theme: ThemeData.light(useMaterial3: true),
+    );
+  }
+}
+
+// TODO: Build a Shrine Theme (103)
+// TODO: Build a Shrine Text Theme (103)
